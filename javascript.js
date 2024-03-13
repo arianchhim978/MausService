8e3734b9ab504a12fd3afa5b0fa193484e8ec99b
arianchhim978 <147645634+arianchhim978@users.noreply.github.com>
2024-03-13 04:01:38 -0400

    added yearly plan and monthly

--- javascript.js
+++ javascript.js
@@ -12,3 +12,25 @@ function highlightButton(clickedButton) {
         }
     });
 }
+
+// Get the checkbox element
+var billingToggle = document.getElementById("billing-toggle");
+
+// Add event listener to detect changes in checkbox state
+billingToggle.addEventListener("change", function() {
+  // Get the pricing elements
+  var monthlyPricing = document.querySelector("#box2 .b");
+  var yearlyPricing = document.querySelector("#box2 .k");
+  var classB = document.querySelector("#box3 .b");
+
+  // If the checkbox is checked, show yearly pricing and hide monthly pricing
+  if (billingToggle.checked) {
+    monthlyPricing.textContent = "$199 /  Yearly"; classB.textContent = "$900/ Yearly";
+
+  } else {
+    // If the checkbox is not checked, show monthly pricing and hide yearly pricing
+    monthlyPricing.textContent = "$19 / Month";
+    classB.textContent = "$99/Month" // Set monthly pricing
+   
+  }
+});
